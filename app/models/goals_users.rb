3a9f9a5431abef1b62e42ed35c90cd990cfff277
Scott <scott.heron@outlook.com>
2016-03-10 11:59:17 -0800

    recreated the database, migration files and models

--- app/models/goals_users.rb
+++
@@ -1,4 +0,0 @@
-class GoalsUsers < ActiveRecord::Base
-  belongs_to :goal
-  belongs_to :user
-end
--- db/migrate/20160310195324_create_places.rb
+++ db/migrate/20160310195324_create_places.rb
@@ -2,8 +2,8 @@ class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
       t.string :name
-      t.string :address
-      t.integer :zip
+      t.string :category
+      t.string :google_id
 
       t.timestamps null: false
     end

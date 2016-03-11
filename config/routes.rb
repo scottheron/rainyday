2750aafe59326e6d8252dac7412b495c43935ad2
steampunklink <nethhorn@gmail.com>
2016-03-10 17:16:00 -0800

    worked on goal view/controller/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'main#index'
 
+  get 'goal' => "goal#show"
+  get 'goal/new' => "goal#new"
+  post 'goal' => "goal#create"
+  delete 'goal' => "goal#destroy"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

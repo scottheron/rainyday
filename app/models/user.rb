d3dc2d62a59893513780c31589034a01bbc7cb74
steampunklink <nethhorn@gmail.com>
2016-03-11 15:30:32 -0800

    worked on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  # belongs_to :zip
+  belongs_to :zip
   # # users.password_hash in the database is a :string
   # include BCrypt
   # def password
@@ -12,4 +12,20 @@ class User < ActiveRecord::Base
   #   @password = Password.create(new_password)
   #   self.password_hash = @password
   # end
+
+  validates :email,
+  presence: true,
+  email: true,
+  uniqueness: {case_sensitive: false}
+
+  validates :password,
+  presence: true,
+  length: {
+    minimum: 7,
+    too_short: "must be greater than %{count} characters"
+  }
+
+  def self.authenticate email, password
+    User.find_by_email(email).try(:authenticate, password)
+  end
 end

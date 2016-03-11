82a26280fcb3ea90dbf5f7fe650793b9840fe0f1
steampunklink <nethhorn@gmail.com>
2016-03-11 13:16:53 -0800

    fixes to signup/zip table

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,21 +5,24 @@ class MainController < ApplicationController
 
   def new
     user = User.new
+    zip = Zip.new
   end
 
   def create
+    puts user_params
     if user_params == nil
       flash[:danger] = "Passwords don't match"
       redirect_to root_path
     else
       user = User.create user_params
+      zip = Zip.create zip_params
       if user.valid?
         session[:user_id] = user.id
         flash[:success] = 'User created and logged in'
         redirect_to root_path
       else
         messages = user.errors.map { |k, v| "#{k} #{v}" }
-        flash[:danger] = messages.join(', ')
+        flash[:danger] = "Something's wrong"
         redirect_to root_path
       end
     end
@@ -27,10 +30,15 @@ class MainController < ApplicationController
 
   private
 
+  def zip_params
+    user_code = params[:zip][:code]
+    params.require(:zip).permit(:code)
+  end
+
   def user_params
-    password = params.require(:user).permit(:password)
-    password_confirmation = params.require(:user).permit(:password_confirmation)
-    if password === password_confirmation
+    password = params[:user][:password]
+    password_confirmation = params[:user][:password_confirmation]
+    if (password == password_confirmation)
       params.require(:user).permit(:name, :email, :password)
     else
 

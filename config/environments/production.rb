f11f41ba1fe3324b660a4ca1356bdcd9d54eeac9
Scott <scott.heron@outlook.com>
2016-03-14 16:43:09 -0700

    error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            'GMAIL_USERNAME',
-    password:             'GMAIL_PASSWORD',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
